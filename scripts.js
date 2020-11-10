a898082c4ce3df45ced12aca170fde1cbdef9c21
minhnguyen303 <minh09az09@gmail.com>
2020-11-10 08:57:36 +0700

    th su dung toan tu js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+var inputWidth = prompt("Enter the width");
+var inputHeight = prompt("Enter the height");
+
+var width = parseInt(inputWidth);
+var height = parseInt(inputHeight);
+
+var area = width * height;
+document.write("The area is: " + area);
